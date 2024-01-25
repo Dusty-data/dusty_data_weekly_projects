0cc7fdfa93d2026256a834a458d44f3909e44bc5
Dusty-data <fsnursen@gmail.com>
2024-01-25 15:43:02 +0100

    Update q9.py

--- wk1/q9.py
+++ wk1/q9.py
@@ -2,11 +2,11 @@
 # (tersten okunduğunda aynı olan) olup olmadığını kontrol eden bir döngü 
 # ve koşullu ifade kombinasyonu nasıl oluşturulur?
 
-word = input("Enter a word that you want to check that is same with the reversed or not:")
+word = input("Enter a word that you want to check that is same with the reversed or not: ")
 
 reversed_word = word[::-1]
 
 if reversed_word == word:
     print(f"The reversed of {word} is the same.")
 else:
-    print("Find another word. The reversed one is different than your original word.")
+    print(f"Find another word. The {reversed_word} is different than {word}.")
