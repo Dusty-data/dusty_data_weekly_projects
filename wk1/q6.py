2e1a01e61bbf8d0cc744240531f86ca29269b677
Dusty-data <fsnursen@gmail.com>
2024-01-25 09:12:48 +0100

    q6 is done test

--- wk1/q6.py
+++ wk1/q6.py
@@ -1,2 +1,17 @@
 # Kullanıcıdan bir sayı alan ve bu sayının 
 # asal olup olmadığını kontrol eden bir Python kodu yazınız.
+ 
+number = int(input("Enter a number that you want to check that is a prime number: "))
+
+if number < 2:
+    print(f"The number {number} that you enter is not a prime number.")
+
+else:
+    for i in range(2, number):
+        if number % i == 0:
+            print(f"The number {number} that you enter is not a prime number.")
+            break
+
+        else:
+            print(f"The number {number} that you enter is a prime number.")
+            break
---
+++ wk1/q7.py
@@ -0,0 +1,2 @@
+# Fibonacci dizisini hesaplayan ve sonucu belirli bir sınıra kadar olan sayıları 
+# içeren bir liste olarak döndüren bir döngü nasıl oluşturulur?
